3f709d4428bee9cd8920f5cd66702bbf0a95c056
rebeccariley <rebecca.a.riley@vanderbilt.edu>
2013-10-30 19:52:09 -0500

    Network updated

--- ProductivityApp/src/org/vandyapps/productivityapp/Network.java
+++ ProductivityApp/src/org/vandyapps/productivityapp/Network.java
@@ -1,10 +1,178 @@
 package org.vandyapps.productivityapp;
+import java.util.*;
 
 public class Network {
+	private int id;
+	private ArrayList<String> user;
+	private ArrayList<String> project;
+	private String name;
+	private String description;
+	
+	
+	/**
+	 * Create a network with a given name and assigned network id.
+	 * @param id
+	 * @param name
+	 */
+	public Network(int id, String name) {
+		if (name.equals("")) {
+			setName("Unnamed Network");
+		}
+		else{
+			setName(name);
+		}
+		
+		if (id<0) {
+			throw new IllegalArgumentException();
+		}
+		this.id=id;
+	}
+	
 
 	/**
-	 * @param args
+	 * Set network name to given value.
+	 * @param name
 	 */
-	//will be used to connect to specific network/server in the cloud
-
+	public void setName(String name) {
+		this.name=name;
+	}
+	
+	/**
+	 * Set network description to a given value.
+	 * @param description
+	 */
+	/* I didn't think that a network description was an essential
+	 * aspect of creating a network.  However, we can add in the
+	 * functionality to set the description when adding a network
+	 * either here or in the GUI (??) later.
+	 */
+	public void setDescription(String description) {
+		this.description=description;
+	}
+	
+	/**
+	 * Returns network name.
+	 * @return
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Returns network description.
+	 * @return
+	 */
+	public String getDescription() {
+		return description;
+	}
+	
+	/**
+	 * Returns network id.
+	 * @return
+	 */
+	/* I'm not sure if there would ever be a reason to retrieve an id.
+	 * If we want to keep it private, that's easy; we'll just delete
+	 * this method.
+	 */
+	public int getID() {
+		return id;
+	}
+	
+	/**
+	 * Add user to array list of users in the network. 
+	 * @param name
+	 * @return--returns true if adding user generates no exceptions,
+	 * false if exception generated
+	 */
+	/* Do we want users to be identified by an id instead?
+	 * That way we could attach other important information
+	 * to them as well--ex. email, work, picture, etc.
+	 * This would preclude people with the same name being
+	 * interpreted as the same user.
+	 */
+	public boolean addUser(String name) {
+		/*try {
+			user.add(name);
+			return true;
+		}
+		finally {
+			return false;
+		}*/
+		if(user.add(name)){
+			return true;
+		}
+		return false;
+		/* This 'finally' is jank and incorrect coding, I'm pretty
+		 * sure, but I don't know what kind of exception we'd
+		 * be throwing when trying to add users.  The finally in
+		 * this case would execute if any type of exception occurs.
+		 * We could easily change this later.
+		 */
+	}
+	
+	/**
+	 * Adds project to array list of projects in the network.
+	 * @param project
+	 * @return--returns true if no exceptions generated, false if
+	 * exception generated
+	 */
+	/* See previous comments about try-finally. Same with a project
+	 * id.
+	 */
+	public boolean addProject(String project) {
+		/*try {
+			this.project.add(project);
+			return true;
+		}
+		finally {
+			return false;
+		}*/
+		if(this.project.add(project)){
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Removes a user from the network.
+	 * @return--returns true if no exceptions generated, false if
+	 * exception generated
+	 */
+	/* Instead of a boolean, we might want to change this to return an
+	 * int depending on various error messages, i.e. 1 for if user does
+	 * not have permission to remove another user, 2 if the user does
+	 * not exist, etc.
+	 * Another way to preclude the "user does not exist" error would be
+	 * to associate users with an id number. 
+	 */
+	public boolean removeUser(String user) {
+		try {
+			this.user.remove(this.user.indexOf(user));
+			// we have problems here if multiple users have the same name
+			return true;
+		}
+		finally {
+			return false;
+		}		
+	}
+	
+	/**
+	 * Removes 
+	 * @param project
+	 * @return--returns true if no exceptions generated, false if
+	 * exception generated
+	 */
+	
+	/* See comments on 'removeUser.'
+	 */
+	public boolean removeProject(String project) {
+		try {
+			this.project.remove(this.project.indexOf(project));
+			// problems if multiple projects have the same name
+			return true;
+		}
+		finally {
+			return false;
+		}
+	}
 }
